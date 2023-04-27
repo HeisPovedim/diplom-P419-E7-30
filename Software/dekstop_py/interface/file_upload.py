595a952e944b7f2484f4cd302fadf5e0d7ce1e23
Mikhail <77213018+HeisPovedim@users.noreply.github.com>
2023-04-27 10:25:14 +0300

    commit

--- Software/dekstop_py/interface/file_upload.py
+++ Software/dekstop_py/interface/file_upload.py
@@ -1,7 +1,7 @@
 import csv
 
-from PyQt6.QtWidgets import QMainWindow, QFileDialog
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PyQt6 import QtCore, QtWidgets
 
 from interface.graph_widget import GraphWidget
 
@@ -17,52 +17,58 @@ class FileUpload(QMainWindow):
         # Инициализация переменных
         self.GraphWidget = None
         
+        # Отрисовка окна
         self.setupUi()
         
     def setupUi(self):
+        """Интерфейс всего окна"""
         
+        # Выравнивание элементов по центру
         centralWidget = QtWidgets.QWidget(parent=self)
-
         verticalLayout = QtWidgets.QVBoxLayout(centralWidget)
 
+        # Создание сетки
         gridLayout = QtWidgets.QGridLayout()
 
+        # кнопка загрузки файла
         file_upload = QtWidgets.QPushButton(parent=centralWidget)
         file_upload.setText("Загрузить файл")
         file_upload.clicked.connect(self.load_data)
-
         gridLayout.addWidget(file_upload, 1, 0, 1, 1)
+        
+        # заголовок
         title = QtWidgets.QLabel(parent=centralWidget)
         title.setText("Выберите файл в формате csv")
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        title.setFont(font)
         title.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter|QtCore.Qt.AlignmentFlag.AlignTop)
-
         gridLayout.addWidget(title, 0, 0, 1, 1)
-        verticalLayout.addLayout(gridLayout)
         
+        # Размещение элементов в окне
+        verticalLayout.addLayout(gridLayout)
         self.setCentralWidget(centralWidget)
         
     def load_data(self):
+        """Функция построение графика и его отображение"""
         
         # Открываем диалоговое окно для выбора файла CSV
         filename, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "", "CSV Files (*.csv)")
 
-        # Читаем данные из файла CSV
-        data = []
-        with open(filename, 'r') as file:
-            reader = csv.reader(file)
-            next(reader)  # Пропускаем заголовок
-            for row in reader:
-                x = float(row[0])
-                y = float(row[1])
-                data.append([x, y])
+        if filename:
+            # Читаем данные из файла CSV
+            data = []
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Пропускаем заголовок
+                for row in reader:
+                    x = float(row[0])
+                    y = float(row[1])
+                    data.append([x, y])
 
-        # Данные для построения графика
-        x_data = [row[0] for row in data]
-        y_data = [row[1] for row in data]
-        
-        # Отрисовка графика
-        self.GraphWidget = GraphWidget(x_data, y_data)
-        self.GraphWidget.show()
+            # Данные для построения графика
+            x_data = [row[0] for row in data]
+            y_data = [row[1] for row in data]
+            
+            # Отрисовка графика
+            self.GraphWidget = GraphWidget(x_data, y_data)
+            self.GraphWidget.show()
+        else:
+            QMessageBox.warning(self, "Ошибка", "Выберите файл!")
